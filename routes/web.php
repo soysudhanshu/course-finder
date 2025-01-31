88df63fb3bd428c71d58a17c486208f7c5b1c111
soysudhanshu <69431877+soysudhanshu@users.noreply.github.com.>
2025-01-31 17:38:47 +0530

    Bugfix: Undefined Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
+
+Route::get(
+    '/login',
+    fn() => response()->json('Unauthorised user Authorization header', Response::HTTP_UNAUTHORIZED)
+)->name('login');
